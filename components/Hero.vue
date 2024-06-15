40772cab1c34c1b11967de19a4753af71774dad2
felixwongong <74048524+felixwongong@users.noreply.github.com>
2024-06-16 02:41:19 +0800

    fix kontra transpile & remove unused component which cause build exception

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,40 +1,40 @@
 <template>
-  <header
-      class="max-w-screen-lg m-16 mx-auto bg-opacity-20"
-  >
-    <GameCamvas class="w-full h-full"/>
-    <div class="text-wrapper flex flex-row justify-center font-bungee text-3xl font-bold">
-      <span style="--i:8">IN</span>
+  <header class="max-w-screen-lg m-16 mx-auto bg-opacity-20">
+    <GameCanvas class="w-full h-full" />
+    <div
+      class="text-wrapper flex flex-row justify-center font-bungee text-3xl font-bold"
+    >
+      <span style="--i: 8">IN</span>
       <span> </span>
-      <span style="--i:9">  P</span>
-      <span style="--i:10">R</span>
-      <span style="--i:11">O</span>
-      <span style="--i:12">G</span>
-      <span style="--i:13">R</span>
-      <span style="--i:14">E</span>
-      <span style="--i:15">S</span>
-      <span style="--i:16">S</span>
+      <span style="--i: 9"> P</span>
+      <span style="--i: 10">R</span>
+      <span style="--i: 11">O</span>
+      <span style="--i: 12">G</span>
+      <span style="--i: 13">R</span>
+      <span style="--i: 14">E</span>
+      <span style="--i: 15">S</span>
+      <span style="--i: 16">S</span>
     </div>
   </header>
 </template>
 
 <style scoped>
-
 span {
   position: relative;
   display: inline-block;
   letter-spacing: 10px;
   animation: waveAnim 3s infinite;
-  animation-delay: calc(.1s * var(--i));
+  animation-delay: calc(0.1s * var(--i));
 }
 
 @keyframes waveAnim {
-  0%, 40%, 100% {
+  0%,
+  40%,
+  100% {
     transform: translateY(0);
   }
   20% {
     transform: translateY(-3rem);
   }
 }
-
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css', '@fortawesome/fontawesome-svg-core/styles.css'],
+  css: [
+    "~/assets/css/main.css",
+    "@fortawesome/fontawesome-svg-core/styles.css",
+  ],
   app: {
     head: {
-      link:[{ rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css', integrity: 'sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==', crossorigin: 'anonymous', referrerpolicy: 'no-referrer'}],
-    }
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css",
+          integrity:
+            "sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==",
+          crossorigin: "anonymous",
+          referrerpolicy: "no-referrer",
+        },
+      ],
+    },
   },
   components: {
     global: true,
-    dirs: ['~/components']
+    dirs: ["~/components"],
   },
   postcss: {
     plugins: {
@@ -17,4 +29,7 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-})
+  build: {
+    transpile: ["kontra"],
+  },
+});
